24188f9287c74a952c9ed23bb4f488d5acdc7bf2
LucDomi <ldominguezsanchez@safareyes.es>
2026-01-20 14:41:30 +0100

    Crud de Opinion creado. Nota importante: NO HACER UN MAKE:CRUD PARA USER, PETA EL PROYECTO Y NO ES LA FORMA CORRECTA.

---
+++ src/Form/OpinionType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Anime;
+use App\Entity\Opinion;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OpinionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('texto')
+            ->add('puntuacion')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('anime', EntityType::class, [
+                'class' => Anime::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Opinion::class,
+        ]);
+    }
+}
