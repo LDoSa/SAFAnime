99358be2d115f7b871800c45c15287abfbf28459
LucDomi <ldominguezsanchez@safareyes.es>
2026-02-16 21:45:22 +0100

    Commit

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -19,30 +18,23 @@ class Category
     #[ORM\Column(length: 100)]
     private ?string $name = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[ORM\Column(type: 'text', nullable: true)]
     private ?string $image = null;
 
-    /**
-     * @var Collection<int, Ranking>
-     */
-    #[ORM\OneToMany(targetEntity: Ranking::class, mappedBy: 'category')]
-    private Collection $rankings;
-
-    /**
-     * @var Collection<int, Anime>
-     */
-    #[ORM\ManyToMany(targetEntity: Anime::class, inversedBy: 'category')]
+    #[ORM\ManyToMany(targetEntity: Anime::class, inversedBy: 'categories')]
     #[ORM\JoinTable(name: 'category_anime')]
-    #[ORM\JoinColumn(name: 'id_category', referencedColumnName: 'id')]
-    #[ORM\InverseJoinColumn(name: 'id_anime', referencedColumnName: 'id')]
-    private Collection $AnimeCategory;
+    private Collection $animes;
+
+    #[ORM\OneToMany(targetEntity: Ranking::class, mappedBy: 'category', orphanRemoval: true)]
+    private Collection $rankings;
 
     public function __construct()
     {
+        $this->animes = new ArrayCollection();
         $this->rankings = new ArrayCollection();
-        $this->AnimeCategory = new ArrayCollection();
     }
 
+    // ---------------- Propiedades ----------------
     public function getId(): ?int
     {
         return $this->id;
@@ -56,7 +48,6 @@ class Category
     public function setName(string $name): static
     {
         $this->name = $name;
-
         return $this;
     }
 
@@ -68,13 +59,33 @@ class Category
     public function setImage(?string $image): static
     {
         $this->image = $image;
-
         return $this;
     }
 
-    /**
-     * @return Collection<int, Ranking>
-     */
+    // ---------------- Animes ----------------
+    public function getAnimes(): Collection
+    {
+        return $this->animes;
+    }
+
+    public function addAnime(Anime $anime): static
+    {
+        if (!$this->animes->contains($anime)) {
+            $this->animes->add($anime);
+            $anime->addCategory($this);
+        }
+        return $this;
+    }
+
+    public function removeAnime(Anime $anime): static
+    {
+        if ($this->animes->removeElement($anime)) {
+            $anime->removeCategory($this);
+        }
+        return $this;
+    }
+
+    // ---------------- Rankings ----------------
     public function getRankings(): Collection
     {
         return $this->rankings;
@@ -86,43 +97,17 @@ class Category
             $this->rankings->add($ranking);
             $ranking->setCategory($this);
         }
-
         return $this;
     }
 
     public function removeRanking(Ranking $ranking): static
     {
         if ($this->rankings->removeElement($ranking)) {
-            // set the owning side to null (unless already changed)
             if ($ranking->getCategory() === $this) {
                 $ranking->setCategory(null);
             }
         }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Anime>
-     */
-    public function getAnimeCategory(): Collection
-    {
-        return $this->AnimeCategory;
-    }
-
-    public function addAnimeCategory(Anime $animeCategory): static
-    {
-        if (!$this->AnimeCategory->contains($animeCategory)) {
-            $this->AnimeCategory->add($animeCategory);
-        }
-
-        return $this;
-    }
-
-    public function removeAnimeCategory(Anime $animeCategory): static
-    {
-        $this->AnimeCategory->removeElement($animeCategory);
-
         return $this;
     }
 }
+
